e05b30cf1fb5971a01d67154569d0fae6aeeebe6
Amit Kumar <amitkh@gmail.com>
2022-03-24 12:32:39 -0400

    cleaned dataset (dropped duplicates and kept peak position)

--- merging.py
+++ merging.py
@@ -1,14 +1,23 @@
 import pandas as pd
+from tqdm import tqdm
 
-ts = pd.Timestamp('1958-08-04')
-charts = pd.DataFrame()
-while ts < pd.Timestamp.now():
-    week = ts.strftime('%Y-%m-%d')
-    path = f'data/{week}.feather'
-    chart = pd.read_feather(path)
-    charts = pd.concat([charts, chart])
-    ts += pd.Timedelta(days=7)
+def merge():
+    ts = pd.Timestamp('1958-08-04')
+    charts = pd.DataFrame()
+    for _ in tqdm(range(3321)):
+        week = ts.strftime('%Y-%m-%d')
+        path = f'data/{week}.feather'
+        chart = pd.read_feather(path)
+        charts = pd.concat([charts, chart])
+        ts += pd.Timedelta(days=7)
 
-charts.reset_index(inplace=True)
-charts.to_feather('charts.feather')
+    charts['rank'] = pd.to_numeric(charts['rank'])
+    charts.reset_index(inplace=True)
+    charts.to_feather('charts_clean.feather')
+    charts = charts.sort_values('rank')
+    charts.drop_duplicates(subset='title', inplace=True)
+    charts.reset_index(inplace=True)
+    charts.to_feather('charts_clean.feather')
 
+if __name__ == '__main__':
+    merge()
