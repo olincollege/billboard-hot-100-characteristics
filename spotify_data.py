f98bc3c997a831158e6898dac8712f4e9c4e51c0
Omar <omar.ym.salih@gmail.com>
2022-03-31 03:10:05 -0400

    Progress Comp Essay Other materials also progressed.

--- spotify_data.py
+++ spotify_data.py
@@ -55,25 +55,58 @@ def get_features(charts):
 
 def clean_data(charts):
     '''
-    Clean data by removing any NaNs and reseting the indexing to run from 0
-    sequentially
+    Clean data by removing any NaNs, reseting the indexing to run from 0
+    sequentially, and resetting the weeks to date_time format.
     '''
     charts = charts.dropna()
     charts.reset_index(drop=True, inplace=True)
     return charts
 
+def get_date_information(charts):
+    '''
+    Add more column information including the decade and the year of the songs,
+    and resets the week column to datetime format.
+
+    Args:
+        charts (DataFrame): Cleaned charts DataFrame containing Track IDs with
+            no NaNs
+
+    Returns:
+        The same DataFrame as the input, but with columns added for the songs'
+            decade, year, and week each song charted, with the week in
+            datetime format.
+    '''
+    decade = []
+    year_info = []
+    for i in range(len(charts.loc[:,"week"])):
+        # Checking the decade of the songs for each song
+        year = str(charts.loc[:,"week"][i])[0:4]
+        decade_year = int(year[2])
+        year_info.append(year)
+        if decade_year in range(5,10):
+            decade.append(f"{decade_year}0's")
+        else:
+            decade.append(f"20{decade_year}0's")
+
+    # Adding the Decade and Year information into the DataFrame
+    charts = charts.assign(decade = decade, year = year_info)
+    # Converting the week column to dates
+    charts['week'] = pd.to_datetime(charts['week'])
+    return charts
 
 def main():
     '''
     If ran directly, read in the charts_merged file generated from merging.py,
     add in the track IDs, drop any NaNs, add columns with the song
-    characteristics, and save the output to charts_clean.feather
+    characteristics and the year information, and save the output to
+    charts_clean.feather.
     '''
     charts = pd.read_feather('charts_merged.feather')
     charts_id = get_track_ids(charts)
     charts_clean = clean_data(charts_id)
     charts_features = get_features(charts_clean)
-    charts_features.to_feather('charts_clean.feather')
+    charts_year_info = get_date_information(charts_features)
+    charts_year_info.to_feather('charts_clean.feather')
 
 
 if __name__ == '__main__':
