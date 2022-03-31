890474e6dab3d30d78454024d7237ba3b5ec86e7
Amit Kumar <amitkh@gmail.com>
2022-03-31 11:05:40 -0400

    lint on test file

--- test_data_cleaning.py
+++ test_data_cleaning.py
@@ -6,22 +6,31 @@ import numpy as np
 from merging import replace_featuring
 from spotify_data import clean_data
 
+
 def test_replace_featuring():
+    '''
+    Test function that replaces instances of ' Featuring' in artist names with a
+    comma
+    '''
     test_df = pd.DataFrame()
     test_df['artist'] = ['Future', 'Future Featuring Drake', 'Featuring']
 
     replaced = replace_featuring(test_df)
-    assert (replaced['artist'] == pd.Series(['Future', 'Future, Drake', 'Featuring'])).all()
+    assert (replaced['artist'] == pd.Series(
+        ['Future', 'Future, Drake', 'Featuring'])).all()
+
 
 def test_clean_data():
-    df = {'rank': [1,1,1], 'title': ['a', 'b', 'c'],
-            'artist': ['a1', 'b1', 'c1'],
-            'week': ['1958-08-04', '1958-08-04', '1958-08-04'],
-            'trackid': ['2qufhsj', np.nan, 'aisfaivu']}
+    '''
+    Test functoin that drops NaN values and resets the index to run sequentially
+    '''
+    test_df = {'rank': [1, 1, 1], 'title': ['a', 'b', 'c'],
+               'artist': ['a1', 'b1', 'c1'],
+               'week': ['1958-08-04', '1958-08-04', '1958-08-04'],
+               'trackid': ['2qufhsj', np.nan, 'aisfaivu']}
 
-    df = pd.DataFrame(df)
-    clean = clean_data(df)
+    test_df = pd.DataFrame(test_df)
+    clean = clean_data(test_df)
 
     assert clean.shape == (2, 5)
-    assert list(clean.index) == [0,1]
-
+    assert list(clean.index) == [0, 1]
